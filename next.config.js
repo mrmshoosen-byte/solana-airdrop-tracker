b8f8c3e642d750558f788ac5af22fc05705f8f78
mrmshoosen-byte <mr.m.s.hoosen@gmail.com>
2026-07-02 01:48:39 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,17 +5,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  experimental: {
-    isrMemoryCacheSize: 50 * 1024 * 1024,
-  },
-  serverRuntimeConfig: {
-    DATABASE_URL: process.env.DATABASE_URL,
-    HELIUS_API_KEY: process.env.HELIUS_API_KEY,
-    TOKEN_MINT: process.env.TOKEN_MINT,
-  },
-  publicRuntimeConfig: {
-    TOKEN_MINT: process.env.TOKEN_MINT,
-  },
 };
 
 module.exports = nextConfig;
